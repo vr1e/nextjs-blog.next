3645a99dc7e83dde709bf03900b3fe4510209dd0
Nikola Vrhovac <vrllee@gmail.com>
2021-08-08 12:28:40 +0200

    convert to typescript

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,17 +1,18 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import Date from '../../components/date';
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
-import Date from '../../components/date';
 import utilStyles from '../../styles/utils.module.css';
 
-export async function getStaticProps({ params }) {
-	const postData = await getPostData(params.id);
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+	const postData = await getPostData(params.id as string);
 	return {
 		props: {
 			postData,
 		},
 	};
-}
+};
 
 export async function getStaticPaths() {
 	const paths = getAllPostIds();
@@ -21,7 +22,15 @@ export async function getStaticPaths() {
 	};
 }
 
-export default function Post({ postData }) {
+export default function Post({
+	postData,
+}: {
+	postData: {
+		title: string;
+		date: string;
+		contentHtml: string;
+	};
+}) {
 	return (
 		<Layout>
 			<Head>
